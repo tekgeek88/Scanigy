3cd663ecd3b7868e19affe16396adbdd1dcb8dc4
tekgeek88 <tekgeek88@yahoo.com>
2015-10-18 12:21:06 -0700

    First push

---
+++ app/src/main/java/com/scanimaniacs/com/scanigy/GetItem.java
@@ -0,0 +1,70 @@
+package com.scanimaniacs.com.scanigy;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * Created by TekGeek88 on 10/18/15.
+ */
+public class GetItem {
+
+    public static String[] getItem(final String newUpc) {
+
+
+        String upc = "";
+        String description = "";
+        String line;
+        String[] itemDetailsRefined = new String[2];
+        final URL url;
+
+
+
+// This code prints the contents of an html file on a server
+        try {
+            // Load a URL into the url variable
+            url = new URL(
+                    "http://api.upcdatabase.org/xml/8353cca56727d3bbb53d875e1f4d5692/"
+                            + upc);
+
+            // Print the information about the loaded url to standard output
+
+            // Open a buffered reader to read the incoming data
+            BufferedReader bin = new BufferedReader(new InputStreamReader(
+                    url.openStream()));
+
+            String[] itemDetails = new String[10];
+
+            int lineNumber = 0;
+            while ((line = bin.readLine()) != null) {
+
+                    int startingChar = line.indexOf('>') + 1;
+                    int endingChar = line.indexOf('<', startingChar);
+                    if (lineNumber == 3) {
+                        upc = (line.substring(startingChar, endingChar));
+                    }
+                    if (lineNumber == 6) {
+                        description = line.substring(startingChar, endingChar);
+                    }
+                    lineNumber++;
+
+            } // end of while loop
+
+    }
+    catch (Exception e){
+        e.printStackTrace();
+    }
+
+
+        itemDetailsRefined[0] = upc;
+        itemDetailsRefined[1] = description;
+
+
+        return itemDetailsRefined;
+
+    }
+
+
+}
---
+++ app/src/main/java/com/scanimaniacs/com/scanigy/Item.java
@@ -0,0 +1,13 @@
+package com.scanimaniacs.com.scanigy;
+
+/**
+ * Created by TekGeek88 on 10/18/15.
+ */
+public class Item {
+
+    String upc;
+
+
+
+
+}
